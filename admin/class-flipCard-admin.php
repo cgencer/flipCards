e0303458d511bb871392783b5c8c334733c5420b
Cem Gencer <o.cem.gencer@gmail.com>
2015-04-06 14:56:12 +0300

    settings page elements are loading from json. OK.

--- admin/class-flipCard-admin.php
+++ admin/class-flipCard-admin.php
@@ -96,21 +96,58 @@ class flipCard_Admin {
 	}
 
 	public function flipCard_AdminInit() {
+function array2json($arr) {
+    $parts = array();
+    $is_list = false;
 
+    //Find out if the given array is a numerical array
+    $keys = array_keys($arr);
+    $max_length = count($arr)-1;
+    if(($keys[0] == 0) and ($keys[$max_length] == $max_length)) {//See if the first key is 0 and last key is length - 1
+        $is_list = true;
+        for($i=0; $i<count($keys); $i++) { //See if each key correspondes to its position
+            if($i != $keys[$i]) { //A key fails at position check.
+                $is_list = false; //It is an associative array.
+                break;
+            }
+        }
+    }
+
+    foreach($arr as $key=>$value) {
+        if(is_array($value)) { //Custom handling for arrays
+            if($is_list) $parts[] = array2json($value); /* :RECURSION: */
+            else $parts[] = '"' . $key . '":' . array2json($value); /* :RECURSION: */
+        } else {
+            $str = '';
+            if(!$is_list) $str = '"' . $key . '":';
+
+            //Custom handling for multiple data types
+            if(is_numeric($value)) $str .= $value; //Numbers
+            elseif($value === false) $str .= 'false'; //The booleans
+            elseif($value === true) $str .= 'true';
+            else $str .= '"' . addslashes($value) . '"'; //All other things
+            // :TODO: Is there any more datatype we should be in the lookout for? (Object?)
+
+            $parts[] = $str;
+        }
+    }
+    $json = implode(',',$parts);
+
+    if($is_list) return '[' . $json . ']';//Return numerical JSON
+    return '{' . $json . '}';//Return associative JSON
+} 
 //		$tpl = $this->loader->load('test'); // equivalent to `file_get_contents(dirname(__FILE__).'/views/foo.mustache');
 //		echo $this->mustache->render($tpl);
 
-		require('views/admin_settings.php');
-
+		$sections = json_decode(file_get_contents(dirname(__FILE__).'/views/partials/admin_settings.json'), true);
+		$fields = array();
+		foreach($sections as $id=>$section) {
+ 			$partial = json_decode(file_get_contents(dirname(__FILE__).'/views/partials/' . $section['id'] . '.json'), true);
+			$fields[ $section['id'] ] = $partial;
+		}
 		$this->settings_api->set_sections( $sections );
 		$this->settings_api->set_fields( $fields );
 		$this->settings_api->admin_init();
-/*
-        $this->settings_api->set_sections( $this->get_settings_sections() );
-        $this->settings_api->set_fields( $this->get_settings_fields() );
-        //initialize settings
-        $this->settings_api->admin_init();
-*/
 
 /*
 		register_setting( 'flipCard_settings', 'flipCard_options', 'flipCard_validate_options' );
