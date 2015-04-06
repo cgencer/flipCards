83a17fb7b0b9715f03c57c8dc3cdd48eb11a7a89
Cem Gencer <o.cem.gencer@gmail.com>
2015-04-06 16:33:38 +0300

    TGM plugin activation dependency management OK

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,125 +1,135 @@
 
-module.exports = function (grunt) {
-	grunt.loadNpmTasks('grunt-wiredep');
-	grunt.loadNpmTasks('grunt-notify');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
+module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-wiredep');
+    grunt.loadNpmTasks('grunt-notify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-curl');
     grunt.loadNpmTasks('grunt-wp-i18n');
+    grunt.loadNpmTasks('grunt-phpcs');
 
-	grunt.initConfig({
-		copy: {
-			dist: {
-				src: 'readme.txt',
-				dest: 'README.md'
-			}
-		},
+    grunt.initConfig({
+        phpcs: {
+            application: {
+                src: ['admin/**/*.php', 'includes/**/*.php']
+            },
+            options: {
+                bin: 'vendor/bin/phpcs',
+                standard: 'Zend'
+            }
+        },
+        copy: {
+            dist: {
+                src: 'readme.txt',
+                dest: 'README.md'
+            }
+        },
         curl: {
             'google-fonts-source': {
                 src: 'https://www.googleapis.com/webfonts/v1/webfonts?key=*******',
                 dest: 'assets/vendor/google-fonts-source.json'
             }
         },
-		wiredep: {
-			app: {
-				src: 'index.html'
-			}
-		}
-		makepot: {
-			target: {
-				options: {
-					include: [
-						'path/to/some/file.php'
-					],
+        wiredep: {
+            app: {
+                src: 'index.html'
+            }
+        },
+        makepot: {
+            target: {
+                options: {
+                    include: [
+                        'path/to/some/file.php'
+                    ],
                     type: 'wp-plugin' // or `wp-theme`
                 }
             }
         },
         jshint: {
-        	files: [
-        		'assets/js/filename.js',
-        		'assets/dynamic/paths/**/*.js'
-        	],
-        	options: {
-        		expr: true,
-        		globals: {
-        			jQuery: true,
-        			console: true,
-        			module: true,
-        			document: true
-        		}
-        	}
+            files: [
+                'assets/js/filename.js',
+                'assets/dynamic/paths/**/*.js'
+            ],
+            options: {
+                expr: true,
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true,
+                    document: true
+                }
+            }
         },
         sass: {
-        	dist: {
-        		options: {
-        			banner: '/*! <%= pkg.name %> <%= pkg.version %> filename.min.css <%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %> */\n',
-        			style: 'compressed'
-        		},
-        		files: [{
-        			expand: true,
-        			cwd: 'assets/scss',
-        			src: [
-        				'*.scss'
-        			],
-        			dest: 'assets/css',
-        			ext: '.min.css'
-        		}]
-        	},
-        	dev: {
-        		options: {
-        			banner: '/*! <%= pkg.name %> <%= pkg.version %> filename.css <%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %> */\n',
-        			style: 'expanded'
-        		},
-        		files: [{
-        			expand: true,
-        			cwd: 'assets/scss',
-        			src: [
-        				'*.scss'
-        			],
-        			dest: 'assets/css',
-        			ext: '.css'
-        		}]
-        	}
+            dist: {
+                options: {
+                    banner: '/*! <%= pkg.name %> <%= pkg.version %> filename.min.css <%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %> */\n',
+                    style: 'compressed'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'assets/scss',
+                    src: [
+                        '*.scss'
+                    ],
+                    dest: 'assets/css',
+                    ext: '.min.css'
+                }]
+            },
+            dev: {
+                options: {
+                    banner: '/*! <%= pkg.name %> <%= pkg.version %> filename.css <%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %> */\n',
+                    style: 'expanded'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'assets/scss',
+                    src: [
+                        '*.scss'
+                    ],
+                    dest: 'assets/css',
+                    ext: '.css'
+                }]
+            }
         },
         uglify: {
-        	dist: {
-        		options: {
-        			banner: '/*! <%= pkg.name %> <%= pkg.version %> filename.min.js <%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %> */\n',
-        			report: 'gzip'
-        		},
-        		files: {
-        			'assets/js/filename.min.js' : [
-        				'assets/path/to/file.js',
-        				'assets/path/to/another/file.js',
-        				'assets/dynamic/paths/**/*.js'
-        			]
-        		}
-        	},
-        	dev: {
-        		options: {
-        			banner: '/*! <%= pkg.name %> <%= pkg.version %> filename.js <%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %> */\n',
-        			beautify: true,
-        			compress: false,
-        			mangle: false
-        		},
-        		files: {
-        			'assets/js/filename.js' : [
-        				'assets/path/to/file.js',
-        				'assets/path/to/another/file.js',
-        				'assets/dynamic/paths/**/*.js'
-        			]
-        		}
-        	}
+            dist: {
+                options: {
+                    banner: '/*! <%= pkg.name %> <%= pkg.version %> filename.min.js <%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %> */\n',
+                    report: 'gzip'
+                },
+                files: {
+                    'assets/js/filename.min.js': [
+                        'assets/path/to/file.js',
+                        'assets/path/to/another/file.js',
+                        'assets/dynamic/paths/**/*.js'
+                    ]
+                }
+            },
+            dev: {
+                options: {
+                    banner: '/*! <%= pkg.name %> <%= pkg.version %> filename.js <%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %> */\n',
+                    beautify: true,
+                    compress: false,
+                    mangle: false
+                },
+                files: {
+                    'assets/js/filename.js': [
+                        'assets/path/to/file.js',
+                        'assets/path/to/another/file.js',
+                        'assets/dynamic/paths/**/*.js'
+                    ]
+                }
+            }
         }
     });
 
-	grunt.registerTask('default', [
+    grunt.registerTask('default', [
         'jshint',
         'uglify:dev',
         'uglify:dist',
--- admin/class-flipCard-admin.php
+++ admin/class-flipCard-admin.php
@@ -1,4 +1,5 @@
 <?php
+require_once( dirname(__FILE__).'/lib/TGM-Plugin-Activation/class-tgm-plugin-activation.php');
 require_once( dirname(__FILE__).'/partials/flipCard-admin-settings.php' );
 require_once( dirname(__FILE__).'/lib/mustachephp/mustache.php' );
 require_once( dirname(__FILE__).'/lib/mustache-wordpress-cache/src/Mustache_Cache_WordPressCache.php' );
@@ -31,7 +32,7 @@ class flipCard_Admin {
 
 	private $plugin_name;
 	private $version;
-    private $settings_api;
+	private $settings_api;
 
 	public function __construct( $plugin_name, $version ) {
 
@@ -40,6 +41,9 @@ class flipCard_Admin {
 		$this->settings_api = new WeDevs_Settings_API;
 
 		define('VERSION', $version);
+
+		add_action( 'tgmpa_register', array( $this, 'flipCard_reqPlugins' ));
+
 
 		$this->mustache = new Mustache_Engine(array(
 			'template_class_prefix' 		=> '__MyTemplates_',
@@ -65,6 +69,46 @@ class flipCard_Admin {
 		add_action( 'admin_init', array( $this, 'flipCard_AdminInit' ) );
 	}
 
+	public function flipCard_reqPlugins() {
+		$plugins = array(
+			array(
+				'name'      => 'BuddyPress',
+				'slug'      => 'buddypress',
+				'required'  => false,
+			)
+		);
+		$config = array(
+			'default_path' => '',                      // Default absolute path to pre-packaged plugins.
+			'menu'         => 'tgmpa-install-plugins', // Menu slug.
+			'has_notices'  => true,                    // Show admin notices or not.
+			'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+			'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+			'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+			'message'      => '',                      // Message to output right before the plugins table.
+			'strings'      => array(
+				'page_title'                      => __( 'Install Required Plugins', 'tgmpa' ),
+				'menu_title'                      => __( 'Install Plugins', 'tgmpa' ),
+				'installing'                      => __( 'Installing Plugin: %s', 'tgmpa' ), // %s = plugin name.
+				'oops'                            => __( 'Something went wrong with the plugin API.', 'tgmpa' ),
+				'notice_can_install_required'     => _n_noop( 'This plugin requires the following plugin: %1$s.', 'This plugin requires the following plugins: %1$s.' ), // %1$s = plugin name(s).
+				'notice_can_install_recommended'  => _n_noop( 'This plugin recommends the following plugin: %1$s.', 'This plugin recommends the following plugins: %1$s.' ), // %1$s = plugin name(s).
+				'notice_cannot_install'           => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.' ), // %1$s = plugin name(s).
+				'notice_can_activate_required'    => _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.' ), // %1$s = plugin name(s).
+				'notice_can_activate_recommended' => _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.' ), // %1$s = plugin name(s).
+				'notice_cannot_activate'          => _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.' ), // %1$s = plugin name(s).
+				'notice_ask_to_update'            => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.' ), // %1$s = plugin name(s).
+				'notice_cannot_update'            => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.' ), // %1$s = plugin name(s).
+				'install_link'                    => _n_noop( 'Begin installing plugin', 'Begin installing plugins' ),
+				'activate_link'                   => _n_noop( 'Begin activating plugin', 'Begin activating plugins' ),
+				'return'                          => __( 'Return to Required Plugins Installer', 'tgmpa' ),
+				'plugin_activated'                => __( 'Plugin activated successfully.', 'tgmpa' ),
+				'complete'                        => __( 'All plugins installed and activated successfully. %s', 'tgmpa' ), // %s = dashboard link.
+				'nag_type'                        => 'updated' // Determines admin notice type - can only be 'updated', 'update-nag' or 'error'.
+			)
+		);
+		tgmpa( $plugins, $config );
+	}
+
 	public function flipCard_Admin() {
 
 		add_options_page( 'flipCard Settings Page', 'flipCard', 'manage_options', 'flipcardsettings', array( $this, 'flipCard_Settings' ) );
@@ -77,11 +121,10 @@ class flipCard_Admin {
 		<div class="wrap">
 			<form name="flipCard_options_form_settings_api" method="post" action="options.php">
 <?php	$this->settings_api->show_navigation();
-        $this->settings_api->show_forms();
+		$this->settings_api->show_forms();
 
 		settings_fields('flipCard_Settings');
 		do_settings_sections( 'flipCard_settings_section' ); ?>
-				<input type="submit" value="Submit" class="button-primary" />
 			</form>
 		</div>
 <?php }
@@ -97,44 +140,44 @@ class flipCard_Admin {
 
 	public function flipCard_AdminInit() {
 function array2json($arr) {
-    $parts = array();
-    $is_list = false;
+	$parts = array();
+	$is_list = false;
 
-    //Find out if the given array is a numerical array
-    $keys = array_keys($arr);
-    $max_length = count($arr)-1;
-    if(($keys[0] == 0) and ($keys[$max_length] == $max_length)) {//See if the first key is 0 and last key is length - 1
-        $is_list = true;
-        for($i=0; $i<count($keys); $i++) { //See if each key correspondes to its position
-            if($i != $keys[$i]) { //A key fails at position check.
-                $is_list = false; //It is an associative array.
-                break;
-            }
-        }
-    }
+	//Find out if the given array is a numerical array
+	$keys = array_keys($arr);
+	$max_length = count($arr)-1;
+	if(($keys[0] == 0) and ($keys[$max_length] == $max_length)) {//See if the first key is 0 and last key is length - 1
+		$is_list = true;
+		for($i=0; $i<count($keys); $i++) { //See if each key correspondes to its position
+			if($i != $keys[$i]) { //A key fails at position check.
+				$is_list = false; //It is an associative array.
+				break;
+			}
+		}
+	}
 
-    foreach($arr as $key=>$value) {
-        if(is_array($value)) { //Custom handling for arrays
-            if($is_list) $parts[] = array2json($value); /* :RECURSION: */
-            else $parts[] = '"' . $key . '":' . array2json($value); /* :RECURSION: */
-        } else {
-            $str = '';
-            if(!$is_list) $str = '"' . $key . '":';
+	foreach($arr as $key=>$value) {
+		if(is_array($value)) { //Custom handling for arrays
+			if($is_list) $parts[] = array2json($value); /* :RECURSION: */
+			else $parts[] = '"' . $key . '":' . array2json($value); /* :RECURSION: */
+		} else {
+			$str = '';
+			if(!$is_list) $str = '"' . $key . '":';
 
-            //Custom handling for multiple data types
-            if(is_numeric($value)) $str .= $value; //Numbers
-            elseif($value === false) $str .= 'false'; //The booleans
-            elseif($value === true) $str .= 'true';
-            else $str .= '"' . addslashes($value) . '"'; //All other things
-            // :TODO: Is there any more datatype we should be in the lookout for? (Object?)
+			//Custom handling for multiple data types
+			if(is_numeric($value)) $str .= $value; //Numbers
+			elseif($value === false) $str .= 'false'; //The booleans
+			elseif($value === true) $str .= 'true';
+			else $str .= '"' . addslashes($value) . '"'; //All other things
+			// :TODO: Is there any more datatype we should be in the lookout for? (Object?)
 
-            $parts[] = $str;
-        }
-    }
-    $json = implode(',',$parts);
+			$parts[] = $str;
+		}
+	}
+	$json = implode(',',$parts);
 
-    if($is_list) return '[' . $json . ']';//Return numerical JSON
-    return '{' . $json . '}';//Return associative JSON
+	if($is_list) return '[' . $json . ']';//Return numerical JSON
+	return '{' . $json . '}';//Return associative JSON
 } 
 //		$tpl = $this->loader->load('test'); // equivalent to `file_get_contents(dirname(__FILE__).'/views/foo.mustache');
 //		echo $this->mustache->render($tpl);
@@ -142,7 +185,7 @@ function array2json($arr) {
 		$sections = json_decode(file_get_contents(dirname(__FILE__).'/views/partials/admin_settings.json'), true);
 		$fields = array();
 		foreach($sections as $id=>$section) {
- 			$partial = json_decode(file_get_contents(dirname(__FILE__).'/views/partials/' . $section['id'] . '.json'), true);
+			$partial = json_decode(file_get_contents(dirname(__FILE__).'/views/partials/' . $section['id'] . '.json'), true);
 			$fields[ $section['id'] ] = $partial;
 		}
 		$this->settings_api->set_sections( $sections );
@@ -202,7 +245,7 @@ function array2json($arr) {
 		extract($data);
 		$options = get_option('flipCard_options'); ?>
 
-    	<select id="time_options" name="sandbox_theme_input_examples[time_options]">
+		<select id="time_options" name="sandbox_theme_input_examples[time_options]">
 			<option value="-default-">Select a template...</option><?php
 			$dir = dirname(plugin_dir_path( __FILE__ )) . '/templates';
 			$dh  = opendir($dir);
@@ -215,7 +258,7 @@ function array2json($arr) {
 			?><option value="-new-">Create new template</option>
 		</select>
 		<?php
-    }
+	}
 
 	public function flipCard_text_field_width($data) {
 		extract($data);
